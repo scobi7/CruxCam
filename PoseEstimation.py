f88eecfbfb43519116f899902b41bfdb613498c7
Malachi Nguyen <nguy3n520@gmail.com>
2024-03-05 13:22:56 -0800

    mar 5th commit- PE.py is only bare code

---
+++ PoseEstimation.py
@@ -0,0 +1,33 @@
+import cv2 as cv
+import mediapipe as  mp 
+import time
+
+#creating poses
+mpDraw = mp.solutions.drawing_utils
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+
+cap = cv.VideoCapture('posevids/DevinDropKnee.MOV')
+pTime = 0
+while True:
+    #loading video with frame rate information and color
+    success, frames = cap.read()
+    framesRGB = cv.cvtColor(frames, cv.COLOR_BGR2RGB)
+    results = pose.process(framesRGB)
+
+    #this will generate numbers for x,y and z axis' as well as visibility
+    #could I use this to generate the angles of the limbs?
+    #print (results.pose_landmarks)
+
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(frames, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        for 
+
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv.putText(frames, str(int(fps)), (70,50), cv.FONT_HERSHEY_PLAIN, 3, (255,0,0), 3)
+    cv.imshow('Image', frames)
+    cv.waitKey(1)
